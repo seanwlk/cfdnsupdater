bdae203cc61297676dcf68f496c4540a68267a7c
seanwlk <nicola_97@rocketmail.com>
2024-05-19 10:32:28 +0200

    Added CLI --ldns to list available DNS for the ZoneID

--- dnsupdater.py
+++ dnsupdater.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 import requests
 import json
 import sys
@@ -29,6 +30,11 @@ class updater:
       myIP = False
     return myIP
   def listCFIdentifiersByZoneID(self, zoneID):
+    headers = {
+      'X-Auth-Email' : self.mail,
+      'Authorization' : f"Bearer {self.authToken}",
+      'Content-Type' : 'application/json'
+    }
     req = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zoneID}/dns_records",headers=headers).json()
     return req
   def updateCFIP(self, ip):
@@ -62,21 +68,37 @@ class updater:
         c = 'NO_IP'
     return str(c)
 
+def runDNSUpdate(m):
+  current_ip = m.readIPfromFile()
+  ip = m.getIP()
 
-mgr = updater(json.load(open('config.json')))
+  if not ip:
+    logger.error("Cannot retrieve current IP address")
+    sys.exit()
+  if ip != current_ip:
+    logger.info(f"IP changed from {current_ip} to {ip}")
+    m.sendHASSnotification("Home IP Changed", f"{current_ip} to {ip}")
+    cf = m.updateCFIP(ip)
+    if cf:
+      m.saveIPtoFile(ip)
+  else:
+    logger.debug("IP did not change")
+    sys.exit()
 
-current_ip = mgr.readIPfromFile()
-ip = mgr.getIP()
-
-if not ip:
-  logger.error("Cannot retrieve current IP address")
-  sys.exit()
-if ip != current_ip:
-  logger.info(f"IP changed from {current_ip} to {ip}")
-  mgr.sendHASSnotification("Home IP Changed", f"{current_ip} to {ip}")
-  cf = mgr.updateCFIP(ip)
-  if cf:
-    mgr.saveIPtoFile(ip)
-else:
-  logger.debug("IP did not change")
-  sys.exit()
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="CloudFlare DNS Updater")
+  parser.add_argument('--ldns', action='store_true', help='List account DNS and identifiers')
+  args = parser.parse_args()
+  mgr = updater(json.load(open('config.json')))
+  if args.ldns:
+    data = mgr.listCFIdentifiersByZoneID(mgr.zoneID)
+    if not data['success']:
+      logger.error("Cannot retreive DNS list: "+str(data['errors']))
+      sys.exit()
+    o = "Identifier\t\t\t\tType\tProxied\tName\n"
+    for d in data['result']:
+      o+=f"{d['id']}\t{d['type']}\t{d['proxied']}\t{d['name'].replace('.'+d['zone_name'],'')}\n"
+    print(o)
+    sys.exit()
+  else:  
+    runDNSUpdate(mgr)
